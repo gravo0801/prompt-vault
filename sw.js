1a1df226eacd87560ddfaa055a5d5097d4a5e0ec
gravo0801 <fnaticdoc@gmail.com>
2026-03-15 21:57:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prompt-vault-v2';
+const CACHE_NAME = 'prompt-vault-v3';
 const ASSETS = ['./', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
